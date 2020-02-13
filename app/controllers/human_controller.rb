79b7ac877eaf77c1cd242b5b8c99a04baf9fdbff
yuyahy <xvto.ya@gmail.com>
2020-02-13 12:04:05 +0000

    Backup

--- app/controllers/human_controller.rb
+++ app/controllers/human_controller.rb
@@ -11,4 +11,9 @@ class HumanController < ApplicationController
       @human = Human.create(human_params)
       redirect_to human_path
     end
+    
+    private
+    def human_params
+      params.require(:human).permit(:weight)
+    end
 end

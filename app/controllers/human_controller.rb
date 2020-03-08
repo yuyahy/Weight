ddcd32e3c78a13d25a10500fa0684052df520652
yuyahy <xvto.ya@gmail.com>
2020-03-08 12:04:47 +0000

    Create SignUp Page

--- app/controllers/human_controller.rb
+++ app/controllers/human_controller.rb
@@ -1,7 +1,11 @@
 class HumanController < ApplicationController
   before_action :logged_in_user, only:[:edit, :update, :destroy]
+  # 必要になるかもしれないので残しておく
+  # Create内でエラーが発生する様になったら
+  # 見返すこと
+  #wrap_parameters :human, include: [:name, :weight, :email, :password, :password_confirmation]
     def new
-      @human = Human.new
+     @human = Human.new
     end
     
     def index
@@ -9,16 +13,26 @@ class HumanController < ApplicationController
     end
     
     def create
-      @human = Human.create(human_params)
-      redirect_to human_path(@human)
+     @human = Human.new(human_params)
+      if @human.save!
+        session[:human_id] = @human.id
+        flash[:notice] = "ユーザー登録が完了しました"
+        redirect_to human_path(@human.id)
+      else
+        render :new
+      end
     end
     
     def show
-      @human = Human.find_by(id: params[:id])
+      @human = Human.find_by(params[:id])
     end
     
     private
+    # human_paramsにしないと
+    # 新規ユーザー登録時に
+    # エラーが発生する
+    # password can't be blank
     def human_params
-      params.require(:human).permit(:weight)
+      params.require(:human).permit(:id,:name,:email,:weight,:password,:password_confirmation, :admin)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root   'human#index'
-  get    '/signup',  to: 'users#new'
+  get    '/signup',  to: 'human#new'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
